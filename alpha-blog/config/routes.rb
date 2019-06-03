b821f89ec66ff7220a232fb76956917c96f012bf
vaibhav singh <singh.vaibhav2011@gmail.com>
2019-06-03 22:13:19 +0530

    signup users from ui

--- alpha-blog/config/routes.rb
+++ alpha-blog/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   root 'pages#home'
   get 'about', to: 'pages#about'
 end
