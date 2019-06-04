e7772d8cee71e7330b7be881ffed3f9db3a1d969
vaibhav singh <singh.vaibhav2011@gmail.com>
2019-06-04 15:57:16 +0530

    complete authentication system

--- alpha-blog/config/routes.rb
+++ alpha-blog/config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :articles
-  get 'signup', to: 'users#new'
-  resources :users, except: [:new]
   root 'pages#home'
   get 'about', to: 'pages#about'
+
+  resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
 end
